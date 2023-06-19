a8091757d171de23e7c443ceafd41f302361eddd
Damanpreet Singh <Damansahota07@gmail.com>
2023-06-19 19:13:45 -0400

    Create Top And Bottom Fragment and give the Color

--- SinghModule1/src/main/java/damanpreet/singh/n01426028/mid/MainActivity.java
+++ SinghModule1/src/main/java/damanpreet/singh/n01426028/mid/MainActivity.java
@@ -2,13 +2,50 @@ package damanpreet.singh.n01426028.mid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textview;
+    private ImageButton imageButton;
+    private int counter = 0;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        textview = findViewById(R.id.damTview);
+        imageButton = findViewById(R.id.imageButton);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
     }
-}
+    @Override
+    protected void onPause() {
+
+
+        super.onPause();
+        counter++; // Increment the counter
+        String counterText = "Counter: " + counter;
+        Toast.makeText(this, counterText, Toast.LENGTH_SHORT).show();
+    }
+
+    }
---
+++ SinghModule1/src/main/java/damanpreet/singh/n01426028/mid/SecondActivity.java
@@ -0,0 +1,29 @@
+package damanpreet.singh.n01426028.mid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // todo: goto back activity from here
+
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
